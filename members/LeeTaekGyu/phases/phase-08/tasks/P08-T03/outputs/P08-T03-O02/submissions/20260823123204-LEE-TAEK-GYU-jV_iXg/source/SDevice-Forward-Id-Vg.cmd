09ee5b5595ec332fd61ad14486d0c3c117178805
LEE-TAEK-GYU <xorrb4798@gmail.com>
2026-08-23 21:32:12 +0900

    Submit P08-T03-O02 result by LEE-TAEK-GYU

---
+++ members/LeeTaekGyu/phases/phase-08/tasks/P08-T03/outputs/P08-T03-O02/submissions/20260823123204-LEE-TAEK-GYU-jV_iXg/source/SDevice-Forward-Id-Vg.cmd
@@ -0,0 +1,68 @@
+File {
+  Grid    = "@tdr@"
+  Plot    = "@tdrdat@"
+  Current = "@plot@"
+  Output  = "@log@"
+}
+
+Electrode {
+  { Name = "bitline" Voltage = 0.0 }
+  { Name = "storage" Voltage = 0.0 }
+  { Name = "gate_m1" Voltage = 0.0 WorkFunction = @WF_LOW@  }
+  { Name = "gate_m2" Voltage = 0.0 WorkFunction = @WF_HIGH@ }
+  { Name = "gate_m3" Voltage = 0.0 WorkFunction = @WF_LOW@  }
+}
+
+Physics {
+  Temperature = @Temp@
+  Fermi
+  EffectiveIntrinsicDensity( OldSlotboom )
+  Mobility( PhuMob HighFieldSaturation )
+  Recombination( SRH Band2Band( Model = NonlocalPath ) )
+}
+
+Plot {
+  eDensity hDensity
+  TotalCurrent/Vector eCurrent/Vector hCurrent/Vector
+  Potential ElectricField/Vector SpaceCharge
+  DopingConcentration DonorConcentration AcceptorConcentration
+  ConductionBand ValenceBand eQuasiFermi hQuasiFermi
+  srhRecombination eBand2BandGeneration hBand2BandGeneration Band2BandGeneration
+}
+
+Math {
+  CoordinateSystem { AsIs }
+  * Full 3D: no Cylindrical(yAxis=0).
+  ExcludeTouchingContactParts
+  Derivatives
+  RelErrControl
+  Digits = 5
+  ErrRef(Electron) = 1.0e8
+  ErrRef(Hole)     = 1.0e8
+  Iterations = 40
+  NotDamped  = 30
+  Method = ILS
+  ExtendedPrecision
+  Extrapolate
+}
+
+Solve {
+  Coupled( Iterations = 50 ){ Poisson }
+  Coupled( Iterations = 75 LineSearchDamping = 1.0e-2 ){ Poisson Electron Hole }
+
+  NewCurrentPrefix = "FAST3D_FORWARD_DrainRamp_"
+  Quasistationary(
+    InitialStep = 0.01 Increment = 1.40 Decrement = 4.0
+    MaxStep = 0.05 MinStep = 1.0e-6
+    Goal { Name = "storage" Voltage = @VdBias@ }
+  ){ Coupled( Iterations = 40 ){ Poisson Electron Hole } }
+
+  NewCurrentPrefix = "FAST3D_ForwardIdVg_"
+  Quasistationary(
+    InitialStep = 0.005 Increment = 1.30 Decrement = 4.0
+    MaxStep = 0.01 MinStep = 1.0e-6
+    Goal { Name = "gate_m1" Voltage = @VgStop@ }
+    Goal { Name = "gate_m2" Voltage = @VgStop@ }
+    Goal { Name = "gate_m3" Voltage = @VgStop@ }
+  ){ Coupled( Iterations = 40 ){ Poisson Electron Hole } }
+}
